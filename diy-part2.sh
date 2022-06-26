c6913ff921e18aff54ed297e6510ae140204291b
Guo-Qing Zhang <zhangptnoone@gmail.com>
2022-06-26 13:30:06 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,8 @@
 #
 
 # Modify default IP
-# sed -i 's/10.10.10.1/192.168.163.1/g' package/base-files/files/bin/config_generate
+sed -i 's/10.10.10.1/192.168.163.1/g' package/base-files/files/bin/config_generate
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='MiAX6'' package/lean/default-settings/files/zzz-default-settings
 
 # 修改连接数
 #sed -i 's/net.netfilter.nf_conntrack_max=.*/net.netfilter.nf_conntrack_max=165535/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
